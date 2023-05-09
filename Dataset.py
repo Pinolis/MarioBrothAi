0a39ba0df29b8be00a328c9ab134b67d7001945d
Carcosa777 <84084943+Carcosa777@users.noreply.github.com>
2023-05-09 14:33:09 +0200

    Add files via upload

---
+++ Dataset.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Dataset with keypoints and des of every usefull block 
+
+"""
+
+dataset = {} #diz containing name of block as key and keypoints and des as values
---
+++ grid.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+# pyton grid opencv2 for image
+import cv2
+
+def blockMatch(block, cell, orb, matcher):
+    MIN_MATCH= 5 #parametro variabile
+    kpCell, desCell = orb.detectAndCompute(cell,None)
+    kpBlock, desBlock = Dataset.getpreBlockData(block)
+    matches = matcher.knnMatch(desCell,desBlock,k=2)
+    good_matches = []
+    for m,n in matches:
+        if m.distance < 0.05 * n.distance:
+            good_matches.append(m)
+    if len(good_matches) >= MIN_MATCH:
+        return True #nella return della funzione se true, il blocco nell immagine diventa quello passato nella funzione
+    
+    
+    
+    
+
+
+
+source = cv2.imread("test.png")
+output_dir = 'C:/Users/4ndr5/OneDrive/Documenti/università/ai_lab/Progetto/tests/'
+block = cv2.imread("C:/Users/4ndr5/OneDrive/Documenti/university/ai_lab/Progetto/assets/brick_block_1.png")
+    
+# Define the number of rows and columns in the grid
+rows = 15
+cols = 16
+
+# Get the dimensions of the image
+height, width = source.shape[:2]
+
+# Calculate the size of each cell in the grid
+cell_width = width // cols
+cell_height = height // rows
+
+# Loop over each cell in the grid and extract its image data
+
+for y in range(rows):
+    for x in range(cols):
+        matched_image=None
+        x1 = x * cell_width
+        y1 = y * cell_height
+        x2 = x1 + cell_width
+        y2 = y1 + cell_height
+        cell = source[y1:y2, x1:x2]
+
+        # campute keypoints and decscriptors of the two images
+        keypoints_cell, des_cell = orb.detectAndCompute(cell, None)
+        
+
+        # matching keypoints through knn
+        # matches = matcher.match(des_cell, des_block)
+        # matched_image= cv2.drawMatches(block,keypoints_block,block,des_block,matches,None)
+        
+        # while True:
+        #     cv2.imshow('block_matching', matched_image)
+        #     matched_image = cv2.resize(matched_image,(1500,800) )
+        #     if cv2.waitKey(1) & 0xFF == ord('q'):
+        #         break
+        
+        
+        
+        # Do something with the cell image data, such as saving it to a file
+        #cv2.imwrite('cell{}{}.png'.format(x,y), cell)
+        
+
