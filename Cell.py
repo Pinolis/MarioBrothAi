5868abaa5bf99ed230faf48dad1821b98a69900b
Fresco2 <84515075+Fresco2@users.noreply.github.com>
2023-05-11 16:00:45 +0200

    Add files via upload

---
+++ Cell.py
@@ -0,0 +1,23 @@
+
+print("cioao")
+
+def Cell():
+    
+    def __init__ ():
+        self.x = None
+        self.y = None
+        self.img = None
+    
+    def setImg(img):
+        self.imgCell = img
+
+    def setX(x):
+        self.x=x
+
+    def setY(y):
+        self.y=y
+
+    def getImgCell(img):
+        return imgCell
+    
+
